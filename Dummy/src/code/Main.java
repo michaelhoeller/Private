f4fbe948e07c5b833b92b9c6268ca5c91212ade2
michaelhoeller <Administrator@192.168.36.108>
2017-12-04 16:16:25 +0100

    weqw

--- Dummy/src/code/Main.java
+++ Dummy/src/code/Main.java
@@ -4,27 +4,22 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class Main {
-    
-    /**
-     * getRandomLastName()
-     * getRandomSureName()
-     * 
-     * @throws FileNotFoundException
-     */
-    
-    // Main Methode
-    public static void main(String[] args) throws FileNotFoundException {
-        Utils utils = new Utils();
-        PrintWriter out = new PrintWriter("TestFile.txt");
-        
-        for (int i = 0; i < utils.getDummyAmount(); i++) {
-            Dummy temp = new Dummy();
-            System.out.println(temp.getSureName() + " " + temp.getLastName());
-            System.out.println(temp.getBirthday());
-            Utils.printDummyToFile(out, temp);
-        }
-        out.close();
-        
-    }
-    
+
+	// Main Methode
+	public static void main(String[] args) throws FileNotFoundException {
+		Utils utils = new Utils();
+
+		PrintWriter out = new PrintWriter("C://Users/Administrator/Desktop/TestFile.txt");
+
+		for (int i = 0; i < utils.getDummyAmount(); i++) {
+			Dummy temp = new Dummy();
+
+			System.out.println(temp.getSureName() + " " + temp.getLastName());
+			System.out.println(temp.getBirthday());
+			Utils.printDummyToFile(out, temp);
+		}
+		out.close();
+
+	}
+
 }
--- Dummy/src/code/Utils.java
+++ Dummy/src/code/Utils.java
@@ -6,52 +6,75 @@ import java.util.Properties;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Utils {
-    
-    private Integer dummyAmount;
-    
-    public Utils() {
-        configure();
-    }
-    
-    public void configure() {
-        Properties props = ResourceFinder.findProperties("dummy.properties");
-        
-        String amount = props.getProperty("numberOfDummys");
-        this.setDummyAmount(Integer.parseInt(amount));
-        System.out.println("Anzahl der zu generierenden Dummys: " + amount);
-    }
-    
-    public static int getRandomAge(int min, int max) {
-        return ThreadLocalRandom.current().nextInt(min, max + 1);
-    }
-    
-    public static Timestamp getRandomTimestamp(String beginTime, String endTime) {
-        long offset = Timestamp.valueOf(beginTime).getTime();
-        long end = Timestamp.valueOf(endTime).getTime();
-        
-        long diff = end - offset + 1;
-        Timestamp rand = new Timestamp(offset + (long) (Math.random() * diff));
-        return rand;
-    }
-    
-    public static void printHeaderToFile(PrintWriter out) {
-        out.println();
-        
-    }
-    
-    // INSERT INTO table_name (column1, column2, column3, ...)
-    // VALUES (value1, value2, value3, ...);
-    
-    public static void printDummyToFile(PrintWriter out, Dummy dummy) {
-        out.println("Insert into Users (USER_NAME, USER_LASTNAME, BIRTHDAY) VALUES (" + dummy.getSureName() + ", " + dummy.getLastName() + ", " + dummy.getBirthday() + ");");
-    }
-    
-    public Integer getDummyAmount() {
-        return dummyAmount;
-    }
-    
-    public void setDummyAmount(Integer dummyAmount) {
-        this.dummyAmount = dummyAmount;
-    }
-    
+
+	private Integer dummyAmount;
+	private String startAge;
+	private String endAge;
+
+	public Utils() {
+		configure();
+	}
+
+	public void configure() {
+		Properties props = ResourceFinder.findProperties("dummy.properties");
+
+		String amount = props.getProperty("numberOfDummys");
+		this.setDummyAmount(Integer.parseInt(amount));
+		System.out.println("Anzahl der zu generierenden Dummys: " + amount);
+		this.setStartAge(amount);
+
+		this.setEndAge(amount);
+
+	}
+
+	public static int getRandomAge(int min, int max) {
+		return ThreadLocalRandom.current().nextInt(min, max + 1);
+	}
+
+	public static Timestamp getRandomTimestamp(String beginTime, String endTime) {
+		long offset = Timestamp.valueOf(beginTime).getTime();
+		long end = Timestamp.valueOf(endTime).getTime();
+
+		long diff = end - offset + 1;
+		Timestamp rand = new Timestamp(offset + (long) (Math.random() * diff));
+		return rand;
+	}
+
+	public static void printHeaderToFile(PrintWriter out) {
+		out.println();
+
+	}
+
+	// INSERT INTO table_name (column1, column2, column3, ...)
+	// VALUES (value1, value2, value3, ...);
+
+	public static void printDummyToFile(PrintWriter out, Dummy dummy) {
+		out.println("Insert into Users (USER_NAME, USER_LASTNAME, BIRTHDAY) VALUES (" + dummy.getSureName() + ", "
+				+ dummy.getLastName() + ", " + dummy.getBirthday() + ");");
+	}
+
+	public Integer getDummyAmount() {
+		return dummyAmount;
+	}
+
+	public void setDummyAmount(Integer dummyAmount) {
+		this.dummyAmount = dummyAmount;
+	}
+
+	public String getEndAge() {
+		return endAge;
+	}
+
+	public void setEndAge(String endAge) {
+		this.endAge = endAge;
+	}
+
+	public String getStartAge() {
+		return startAge;
+	}
+
+	public void setStartAge(String startAge) {
+		this.startAge = startAge;
+	}
+
 }
