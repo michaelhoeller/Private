df671bb246a5d2ae81dc797ff74b245d47c14282
michaelhoeller <Administrator@192.168.36.108>
2017-12-05 11:32:05 +0100

    <3

--- Dummy/src/code/Main.java
+++ Dummy/src/code/Main.java
@@ -7,24 +7,21 @@ public class Main {
 
 	// Main Methode
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println("Started");
+
 		Utils utils = new Utils();
 
 		PrintWriter out = new PrintWriter(utils.getOutPutLoc());
 
 		Utils.printHeaderToFile(out, utils.getDummyAmount());
+		System.out.println("Generating " + utils.getDummyAmount() + " Dummys @ " + utils.getOutPutLoc());
 		for (int i = 0; i < utils.getDummyAmount(); i++) {
 			Dummy temp = new Dummy(utils);
 			Utils.printDummyToFile(out, temp);
-			// if ((utils.getDummyAmount() / 100) % (i + 1) == 0) {
-			// System.out.print(".");
-			// }
 		}
-		System.out.println();
+
 		Utils.printEndingToFile(out);
 		out.close();
 		System.out.println("Finished");
-
 	}
 
 }
